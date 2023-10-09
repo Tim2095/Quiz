80f9d51208ca69a9efa00bd836c42e1cf6c1e73f
Timur Mosenkov <timur.mosenkov@gmail.com>
2023-10-09 19:44:10 +0300

    Make input reusable

--- src/layout/UserNameInput.tsx
+++ src/layout/UserNameInput.tsx
@@ -1,28 +1,39 @@
-import classes from './UserNameInput.module.css'
-import { useNavigate } from 'react-router-dom'
-import { useRef } from 'react'
-import { useDispatch } from 'react-redux'
-import { quizActions } from '../store/quiz-slice'
+import classes from "./UserNameInput.module.css";
+import { useNavigate } from "react-router-dom";
+import { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { quizActions } from "../store/quiz-slice";
+import Input from "../ui/Input";
 const UserNameInput: React.FC = () => {
-  const dispatch = useDispatch()
-  const userInput = useRef<HTMLInputElement>(null)
+  const dispatch = useDispatch();
+  const userInput = useRef<HTMLInputElement>(null);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleStartClick = (event: React.FormEvent) => {
-    event.preventDefault()
-    const userName = userInput.current?.value 
-    navigate('/category')
-    dispatch(quizActions.getUserName(userName))
-  }
+    event.preventDefault();
+    const userName = userInput.current?.value;
+    navigate("/category");
+    dispatch(quizActions.getUserName(userName));
+  };
 
   return (
-    <form className={classes.form} onSubmit={handleStartClick}>
-      <label htmlFor="text">Enter Your Name</label>
-      <input type="text" ref={userInput} id="text" placeholder='Your Name' className={classes['name-inp']}  />
-      <button className={classes['start-btn']}>Start</button>
+    // <form className={classes.form} onSubmit={handleStartClick}>
+    //   <label htmlFor="text">Enter Your Name</label>
+    //   <input type="text" ref={userInput} id="text" placeholder='Your Name' className={classes['name-inp']}  />
+    //   <button className={classes['start-btn']}>Start</button>
+    // </form>
+    <form className={classes.form}>
+      <Input label="Enter Your Name"
+        input={{
+          type: "text",
+          id: "name",
+          placeholder: "Enter your name",
+          className: classes["name-inp"],
+        }}
+      />
     </form>
-  )
-}
+  );
+};
 
-export default UserNameInput
+export default UserNameInput;
---
+++ src/ui/Input.tsx
@@ -0,0 +1,25 @@
+import React, { forwardRef } from "react";
+
+interface InputProps {
+  className?: string;
+  label: string;
+  input: {
+    type: string;
+    id: string;
+    placeholder: string;
+    className?: string;
+  };
+}
+
+const Input: React.FC<InputProps> = forwardRef<HTMLInputElement, InputProps>(
+  (props, ref) => {
+    return (
+      <>
+        <label htmlFor={props.input.id}>{props.label}</label>
+        <input {...props.input} ref={ref} />
+      </>
+    );
+  }
+);
+
+export default Input;
