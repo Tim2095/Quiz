a6925318755b39b38370407608b39fe391f52a00
Timur Mosenkov <timur.mosenkov@gmail.com>
2023-10-16 21:46:39 +0300

    remove no needed content from home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,16 +1,9 @@
 import MainHeader from "../layout/MainHeader";
 import UserNameInput from "../layout/UserNameInput";
 import classes from "./HomaPage.module.css";
-import { useSelector } from "react-redux";
-
-interface QUIZ {
-  quiz: {
-    userName: string
-  }
-}
 
 const HomePage = () => {
-  const quizUserName = useSelector((state: QUIZ) => state.quiz.userName)
+ 
   return (
     <main className={classes.main}>
       <MainHeader />
